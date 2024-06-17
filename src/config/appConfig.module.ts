d148f7bff973be40d4dcb60a8bb6a71e9e5692f9
ryongmi <62281754+ryongmi@users.noreply.github.com>
2024-06-17 12:06:58 +0900

    refactor 사용하지 않는 파일 삭제

--- src/config/appConfig.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { appConfig } from './appConfiguration';
-
-@Module({
-  imports: [ConfigModule.forRoot(appConfig)],
-  exports: [ConfigModule],
-})
-export class AppConfigModule {}
--- src/sse/sse.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SseController } from './sse.controller';
-
-@Module({
-  controllers: [SseController],
-})
-export class SseModule {}
