0874496ddf3e579819e12d8e55a744f4bf782cbf
ryongmi <62281754+ryongmi@users.noreply.github.com>
2024-06-17 12:12:43 +0900

    feat config 세팅 후 port 값 활용

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setNestApp } from './setNestApp';
 import { setSwagger } from './setSwagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setNestApp(app);
   setSwagger(app);
-  await app.listen(+process.env.PORT);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
+
+  await app.listen(port);
 }
 bootstrap();
