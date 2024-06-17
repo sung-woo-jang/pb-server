dc3af1f98e526fb6344add4d95b6a461a7f7ffa6
ryongmi <62281754+ryongmi@users.noreply.github.com>
2024-06-17 12:10:28 +0900

    feat naver oauth 로그인 도중 state 검사 guard 추가

---
+++ src/modules/user/guards/oauth-state.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { isWithinMinutes } from '../../../common/utils/date-diff-checker';
+import { AuthException } from '../../../exception';
+
+@Injectable()
+export class OAuthStateGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    if (!request.session.hasOwnProperty('stateCheck')) {
+      // stateCheck이 없으면 요청 반환
+      throw AuthException.authStateNotFound();
+    }
+
+    const { state: callbackState } = request.query;
+    const { state: serverState, createAt } = request.session.stateCheck;
+
+    if (!(callbackState === serverState && isWithinMinutes(new Date(), new Date(createAt), 5))) {
+      // state값이 다르거나, 5분 이내 요청이 아닐경우 요청 반환
+      delete request.session['stateCheck'];
+
+      throw AuthException.authStateNotExist();
+    }
+
+    return true;
+  }
+}
