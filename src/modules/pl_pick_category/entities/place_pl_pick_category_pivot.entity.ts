0300129b2e41453b767be515c28b9d5b5b5eb792
sungwoo <54757435+sung-woo-jang@users.noreply.github.com>
2024-07-15 21:58:01 +0900

    feat: 플픽&장소 피벗테이블 컬럼 추가

--- src/modules/pl_pick_category/entities/place_pl_pick_category_pivot.entity.ts
+++ src/modules/pl_pick_category/entities/place_pl_pick_category_pivot.entity.ts
@@ -1,7 +1,8 @@
 import { BaseEntityIncrement } from '@common/entities/base.entity';
-import { Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
 import { Place } from '../../place/entities/place.entity';
 import { PlPickCategory } from './pl_pick_category.entity';
+import { IsUrl } from 'class-validator';
 
 @Entity()
 export class PlacePlPickCategoryPivot extends BaseEntityIncrement {
@@ -10,4 +11,14 @@ export class PlacePlPickCategoryPivot extends BaseEntityIncrement {
 
   @ManyToOne(() => PlPickCategory, (plPickCategory) => plPickCategory.placePlPickCategoryPivots)
   plPickCategory: PlPickCategory;
+
+  @Column({ nullable: true })
+  memo: string;
+
+  @Column({ nullable: true })
+  alias: string;
+
+  @Column({ nullable: true })
+  @IsUrl()
+  link: string;
 }
