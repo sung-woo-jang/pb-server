173431c6cff79f64ac5e97f979581a914b82d7e5
ryongmi <62281754+ryongmi@users.noreply.github.com>
2024-07-27 14:02:29 +0900

    feat - 게시글 에러메시지 추가

--- src/exception/post.exception.ts
+++ src/exception/post.exception.ts
@@ -4,4 +4,8 @@ export class PostException {
   static notFound(): HttpException {
     return new NotFoundException('게시글을 찾을 수 없습니다.');
   }
+
+  static postAndUserNotFound(): HttpException {
+    return new NotFoundException('사용자나 게시글을 찾을 수 없습니다.');
+  }
 }
