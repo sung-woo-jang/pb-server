dd8a06a8806174b5a91b01ae9f7876281e147694
sungwoo <54757435+sung-woo-jang@users.noreply.github.com>
2024-09-23 18:10:27 +0900

    feat: 플픽 추가 로직 변경

--- src/modules/place_pick/dto/request/create-place_pick.dto.ts
+++ src/modules/place_pick/dto/request/create-place_pick.dto.ts
@@ -1,10 +1,12 @@
 import { PickType } from '@nestjs/swagger';
 import { PlacePick } from '../../entities/place_pick.entity';
+import { ArrayMinSize, IsArray, IsNumber } from 'class-validator';
+import { Expose } from 'class-transformer';
 
-export class CreatePlacePickDto extends PickType(PlacePick, [
-  'memo',
-  'alias',
-  'link',
-  'pl_pick_category_id',
-  'place_id',
-] as const) {}
+export class CreatePlacePickDto extends PickType(PlacePick, ['memo', 'alias', 'link', 'place_id'] as const) {
+  @IsArray()
+  @ArrayMinSize(1)
+  @IsNumber({}, { each: true })
+  @Expose()
+  pl_pick_category_ids: number[];
+}
--- src/modules/place_pick/place_pick.service.ts
+++ src/modules/place_pick/place_pick.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePlacePickDto } from './dto/request/create-place_pick.dto';
-import { PlacePickBuilder } from '../../builder/place_pick.builder';
-import { DataSource } from 'typeorm';
+import { DataSource, In } from 'typeorm';
 import { PlacePickRepository } from './place_pick.repository';
 import { PlPickCategoryRepository } from '../pl_pick_category/pl_pick_category.repository';
 import { PlaceService } from '../place/services/place.service';
 import { User } from '../user/entities';
 import { DeletePlacePickRequestDto } from './dto/request/delete-place_pick-request.dto';
+import { PlacePick } from './entities/place_pick.entity';
 
 @Injectable()
 export class PlacePickService {
@@ -16,22 +16,42 @@ export class PlacePickService {
     private readonly placePickRepository: PlacePickRepository,
     private readonly placeService: PlaceService
   ) {}
-  async createPlacePick({ place_id, link, memo, alias, pl_pick_category_id }: CreatePlacePickDto) {
+  async createPlacePick({ place_id, link, memo, alias, pl_pick_category_ids }: CreatePlacePickDto) {
     return await this.dataSource.transaction(async (manager) => {
       const place = await this.placeService.findPlaceById(place_id);
-      const plPickCategory = await this.plPickCategoryRepository.findOne({ where: { id: pl_pick_category_id } });
+      const plPickCategories = await this.plPickCategoryRepository.findBy({ id: In(pl_pick_category_ids) });
 
-      // place_pick
-      const placePick = new PlacePickBuilder().setMemo(memo).setLink(link).setAlias(alias).build();
+      // 1. 기존의 모든 PlacePick 찾기
+      const existingPlacePicks = await manager.getRepository(PlacePick).find({
+        where: { place: { id: place_id } },
+        relations: ['plPickCategory'],
+      });
 
-      return await this.placePickRepository.createPlacePick(
-        {
-          ...placePick,
-          place,
-          plPickCategory,
-        },
-        manager
-      );
+      // 2. 삭제해야 할 PlacePick 찾기 및 삭제
+      const categoriesToKeep = new Set(pl_pick_category_ids);
+      const placePicksToDelete = existingPlacePicks.filter((pp) => !categoriesToKeep.has(pp.plPickCategory.id));
+      await manager.getRepository(PlacePick).remove(placePicksToDelete);
+
+      // 3. 새로운 PlacePick 생성 또는 기존 PlacePick 업데이트
+      const results = [];
+      for (const plPickCategory of plPickCategories) {
+        let placePick = existingPlacePicks.find((pp) => pp.plPickCategory.id === plPickCategory.id);
+
+        if (placePick) {
+          // 기존 PlacePick 업데이트
+          placePick.memo = memo;
+          placePick.link = link;
+          placePick.alias = alias;
+        } else {
+          // 새 PlacePick 생성
+          placePick = this.placePickRepository.create({ memo, link, alias, place, plPickCategory });
+        }
+
+        placePick = await this.placePickRepository.createPlacePick(placePick, manager);
+        results.push(placePick);
+      }
+
+      return results;
     });
   }
 
@@ -49,8 +69,6 @@ export class PlacePickService {
   }
 
   async deletePlacePick(deletePlacePickRequestDto: DeletePlacePickRequestDto) {
-    const result = await this.placePickRepository.deletePlacePick(deletePlacePickRequestDto);
-    console.log(result);
-    return result;
+    return await this.placePickRepository.deletePlacePick(deletePlacePickRequestDto);
   }
 }
